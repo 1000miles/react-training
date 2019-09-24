28a368d08671cbde2af980df6df933ca8ed914d3
Thuy Le <gitnap@mailbox.org>
2019-09-24 10:52:32 +0200

    Iteration 1: Components done.

---
+++ starter-code/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import './IdCard';
+import IdCard from './IdCard';
+
+class App extends Component {
+  render() {
+
+
+    return (
+      <div className="App">
+        <IdCard
+          lastName='Doe'
+          firstName='John'
+          gender='male'
+          height={178}
+          // Fix: Change date object to string
+          birth={new Date("1992-07-14").toString()}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+
+        <IdCard
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={172}
+          birth={new Date("1988-05-11").toString()}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ starter-code/src/IdCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const IdCard = props => {
+
+  console.log(props)
+  
+  const {lastName, firstName, gender, height, birth, picture} = props;
+
+  return (
+    <div>
+      <img src={picture} />
+      <div>
+        <p>First name: {firstName}</p>
+        <p>Last name: {lastName}</p>
+        <p>Gender: {gender}</p>
+        <p>Height: {height}</p>
+        <p>Birth: {birth}</p>
+      </div>
+    </div>
+  )
+}
+
+export default IdCard;
